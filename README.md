9951e757671717e9b78c8d0257f7c998127c4fbf
Burning_PM <78918118+burning-pm@users.noreply.github.com>
2023-01-18 08:13:37 -0800

    Add Invoke-AtomicEmulations Script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Emulation-Tools
+
+This repo contains various scripts and tools that may be useful for assiting in emulating threats. 
+
+## Invoke-AtomicEmulations
+This is a wrapper script for Invoke-Atomic, it reads YAML files that contain various Atomics that attempt to emulate various threats. To get started, follow the instructions to install [Invoke-Atomic](https://github.com/redcanaryco/invoke-atomicredteam/wiki/Installing-Invoke-AtomicRedTeam) and then run the test!
+
+```PowerShell
+.\Invoke-AtomicEmulation.ps1 -PathToEmulationPlan ..\Emulations\Phosphorus.yml
+```
+
+Current implemented parameters:
+- `PathToAtomicsFolder`
+    - The default Atomics paths are based on the installation of Invoke-Atomic, this can be overridden with this parameter.
+- `PathToInvoke`
+    - The default Invoke-Atomic path is based on the installation of Invoke-Atomic, this can be overriden with this parameter.
+- `PathToEmulationPlan`
+    - This is the path to the specific emulation YAML file. Currently, only one emulation can be executed at a time.
+- `Cleanup`
+    - If you wish for cleanup commands to be executed after the emulation finishes, use this parameter with `-Cleanup $true`
---
+++ Scripts/Invoke-AtomicEmulation.ps1
@@ -0,0 +1,117 @@
+﻿<#
+.SYNOPSIS
+    Execute an adversary emulation plan utilizing Atomic Red Team
+.DESCRIPTION
+    Reads a YAML file containing various Atomic tests used to emulate technqiues used by adversaries and commodity malware. 
+.EXAMPLE Run the emulation plan and specify custom path to Invoke-Atomic and Atomics
+    PS/> Invoke-AtomicEmulation -PathToInvoke C:\Path\To\Invoke -PathToAtomicsFolder C:\Path\To\Atomics
+.EXAMPLE Run the emulation plan and run clean up command
+    PS/> Invoke-AtomicEmulation -Cleanup $true
+.NOTES
+    Create Atomic Tests from yaml files described in Atomic Red Team. https://github.com/redcanaryco/atomic-red-team/tree/master/atomics
+.LINK
+    Installation and Usage Wiki: https://github.com/redcanaryco/invoke-atomicredteam/wiki
+    Github repo: https://github.com/redcanaryco/invoke-atomicredteam
+#>
+
+param(
+    [Parameter(Mandatory = $false,
+    ParameterSetName = 'technique')]
+    [String]
+    $PathToAtomicsFolder = $( if ($IsLinux -or $IsMacOS) { $Env:HOME + "/AtomicRedTeam/atomics" } else { $env:HOMEDRIVE + "\AtomicRedTeam\atomics" }),
+
+    [Parameter(Mandatory = $false,
+    ParameterSetName = 'technique')]
+    [String]
+    $PathToInvoke = $( if ($IsLinux -or $IsMacOS) { $Env:HOME + "/AtomicRedTeam/invoke-atomicredteam" } else { $env:HOMEDRIVE + "\AtomicRedTeam\invoke-atomicredteam" }),
+    
+    [Parameter(Mandatory = $false,
+    ParameterSetName = 'technique')]
+    [string]
+    $Cleanup = $false,
+
+    [Parameter(Mandatory = $true,
+    ParameterSetName = 'technique')]
+    [string]
+    $PathToEmulationPlan = $( if ($IsLinux -or $IsMacOS) { $Env:HOME + "/AtomicRedTeam/emulations" } else { $env:HOMEDRIVE + "\AtomicRedTeam\emulations" })
+)
+
+if(Get-Module -ListAvailable -Name powershell-yaml)
+{   import-module powershell-yaml }
+else
+{   Install-Module -Name powershell-yaml -Scope CurrentUser }
+
+function Invoke-AtomicsCheck()
+{
+    $PathToAtomicsFolder = (Resolve-Path $PathToAtomicsFolder).Path
+    if (Test-Path -Path $PathToAtomicsFolder -IsValid)
+    { Write-Host -ForegroundColor Cyan "PathToAtomicsFolder = $PathToAtomicsFolder" }
+    else
+    { Write-Host -ForegroundColor Red "Unable to validate path to Atomic Red Team" }
+}
+
+function Invoke-IARTCheck()
+{
+    $PathToInvoke = (Resolve-Path $PathToInvoke).Path
+    if (Test-Path -Path $PathToInvoke -IsValid)
+    { 
+        Write-Host -ForegroundColor Cyan "PathToInvoke-Atomic = $PathToInvoke"; 
+        Import-Module $PathToInvoke\Invoke-AtomicRedTeam.psm1
+    }
+    else 
+    {
+        $answer = Read-Host -Prompt "Would you like to install Invoke-AtomicRedTeam and Atomics? Y/N"
+        if($answer == "Y" -or $answer == "y")
+        {
+            Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
+            Install-AtomicRedTeam -getAtomics
+        }
+        else 
+        {   break;  }
+    }
+}
+
+Function Invoke-AtomicPreReqCheck($yaml)
+{
+    $yaml.atomics | %{
+        If($_.input_arguments.count -gt 0)
+        {   Invoke-AtomicTest $_.technique -TestGuids $_.guid -InputArgs $_.input_arguments -GetPrereqs }
+        else
+        {   Invoke-AtomicTest $_.technique -TestGuids $_.guid -GetPrereqs }
+    }
+}
+
+Function Invoke-CleanUp($yaml)
+{
+    $yaml.atomics | %{
+        If($_.input_arguments.count -gt 0)
+        {   Invoke-AtomicTest $_.technique -TestGuids $_.guid -InputArgs $_.input_arguments -Cleanup }
+        else
+        {   Invoke-AtomicTest $_.technique -TestGuids $_.guid -Cleanup}
+    }
+}
+
+if (Test-Path -Path $PathToEmulationPlan -PathType Leaf)
+{    
+    [string[]]$fileContent = Get-Content $PathToEmulationPlan
+    $content = ''
+    foreach ($line in $fileContent) { $content = $content + "`n" + $line }
+    $yaml = ConvertFrom-YAML $content
+}
+else
+{ Write-Host -ForegroundColor Red "Unable to validate path to emulation file"; break; }
+
+Invoke-IARTCheck
+Invoke-AtomicsCheck($yaml)
+Invoke-AtomicPreReqCheck($yaml)
+
+Write-Host -ForegroundColor Yellow "`nExecuting emulation of: " $yaml.emulation_plan "'n"
+$yaml.atomics | %{
+    If($_.input_arguments.count -gt 0)
+    {   Invoke-AtomicTest $_.technique -TestGuids $_.guid -InputArgs $_.input_arguments }
+    else
+    {   Invoke-AtomicTest $_.technique -TestGuids $_.guid }
+}
+
+If($Cleanup -eq $true)
+{ Invoke-CleanUp($yaml) }
